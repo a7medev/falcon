374c4704fc2e8e285109f6239a60473e57d26bfc
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2025-02-12 10:30:40 +0200

    feat: make dummy http server

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "net.h"
 
 int main(void) {
-    printf("Hello, World!\n");
+    Listener listener;
+
+    const int PORT = 4221;
+    int result = Listen(&listener, PORT);
+    if (result != 0) {
+        fprintf(stderr, "Failed to listen on port %d\n", PORT);
+        exit(1);
+    }
+
+    printf("Listening on port %d\n", PORT);
+
+    while (true) {
+        Connection connection;
+        if (Accept(&listener, &connection) != 0) {
+            fprintf(stderr, "Failed to accept connection\n");
+            continue;
+        }
+
+        printf("Accepted connection %s:%d\n", connection.address, connection.port);
+
+        const char buf[] = "HTTP/1.1 200 OK\r\n\r\n";
+        Write(&connection, buf, sizeof(buf) - 1);
+        Close(&connection);
+    }
+
     return 0;
 }
